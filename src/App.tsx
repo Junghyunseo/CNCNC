6b630a97bba68f543bae979bde313b947ec50781
hyunseo <hyunseo0412@naver.com>
2025-02-13 10:59:47 +0900

    feat: fix App

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Home />
-      <div className="w-96 bg-white shadow rounded m-5">w-96</div>
-      <div className="w-80 bg-white shadow rounded m-5">w-80</div>
-      <div className="w-72 bg-white shadow rounded m-5">w-72</div>
-      <div className="w-64 bg-white shadow rounded m-5">w-64</div>
-      <div className="w-60 bg-white shadow rounded m-5">w-60</div>
-      <div className="w-56 bg-white shadow rounded m-5">w-56</div>
-      <div className="w-52 bg-white shadow rounded m-5">w-52</div>
-      <div className="w-48 bg-white shadow rounded m-5">w-48</div>
     </div>
   );
 };
